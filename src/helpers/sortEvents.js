01d4f0ea013151dacc165a7021d9897383ad9fbd
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-09-10 13:34:36 +0100

    Update sort event

--- src/helpers/sortEvents.js
+++ src/helpers/sortEvents.js
@@ -1,13 +1,17 @@
 function sortEvents(events) {
   const sortedEvent = {};
   if (events) {
-    events.data.forEach((event) => {
-      const eventTypeKey = event.linkedEventTypeName || event.typeName;
-      if (!sortedEvent[eventTypeKey]) {
-        sortedEvent[eventTypeKey] = [];
-      }
-      sortedEvent[eventTypeKey] = [...sortedEvent[eventTypeKey], event];
-    });
+    events.data
+      .sort((item) => {
+        return !item.linkedEventTypeName ? 1 : -1;
+      })
+      .forEach((event) => {
+        const eventTypeKey = event.linkedEventTypeName || event.typeName;
+        if (!sortedEvent[eventTypeKey]) {
+          sortedEvent[eventTypeKey] = [];
+        }
+        sortedEvent[eventTypeKey] = [...sortedEvent[eventTypeKey], event];
+      });
   }
   return sortedEvent;
 }
