b4fac78847e9079e2fbd9913a6f349af969d6ac2
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-09-11 20:55:22 +0100

    Update event list element

--- src/components/events-list/EventsListItem.jsx
+++ src/components/events-list/EventsListItem.jsx
@@ -1,23 +1,39 @@
 import { useState } from 'react';
-import { List } from 'antd';
+import { List, Button } from 'antd';
 import PropTypes from 'prop-types';
 
-import { MarketPopover } from '../common';
+import Modal from '../modal/Modal';
+import Outcome from '../outcome/Outcome';
 import wsClient from '../../services/wsClient';
 
 
 function EventsListItem({ data }) {
   const [market, setMarket] = useState(null);
   const [outcomes, setOutcomes] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
 
   async function fetchMarket() {
     const event = await wsClient.getEvent(data.eventId);
     const marketData = await wsClient.getMarket(event.data.markets[0]);
-    const outComesData = Promise.all(marketData.data.outcomes.map(async (outcome) => {
-      return wsClient.getOutcome(outcome);
-    }));
-    setMarket(marketData.data);
+    const outComesData = Promise.all(
+      marketData.data.outcomes.map((outcome) => {
+        return wsClient.getOutcome(outcome);
+      }));
     setOutcomes(await outComesData);
+    setMarket(marketData.data);
+  }
+
+  function handleMarketButtonClick() {
+    setModalOpen(true);
+    fetchMarket();
+  }
+
+  function renderOutcomes() {
+    return outcomes.map((item) => {
+      return (
+        <Outcome key={item.data.outcomeId + item.data.marketId} outcomeData={item.data.price} />
+      );
+    });
   }
 
   return (
@@ -25,8 +41,10 @@ function EventsListItem({ data }) {
       <p>
         {data.name}
       </p>
-      <button onClick={fetchMarket}>fetch</button>
-      <MarketPopover />
+      <Button type="primary" onClick={handleMarketButtonClick}>See primary market</Button>
+      <Modal visible={modalOpen}>
+        {outcomes ? renderOutcomes() : null}
+      </Modal>
     </List.Item>
   );
 }
@@ -36,3 +54,4 @@ EventsListItem.propTypes = {
 };
 
 export default EventsListItem;
+
