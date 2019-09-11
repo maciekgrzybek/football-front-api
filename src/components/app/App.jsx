dcc03000812ed327c04786ac3b760c878bbd17b0
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-09-11 21:55:25 +0100

    Add odds context

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,18 +3,21 @@ import 'antd/dist/antd.css';
 import Header from '../header/Header';
 import Main from '../main/Main';
 import EventsListWrapper from '../events-list/EventsListWrapper';
+import { OddsProvider } from '../../contexts/oddsContext';
 
 import './App.css';
 
 
 function App() {
   return (
-    <div className="page-wrapper">
-      <Header />
-      <Main>
-        <EventsListWrapper />
-      </Main>
-    </div>
+    <OddsProvider>
+      <div className="page-wrapper">
+        <Header />
+        <Main>
+          <EventsListWrapper />
+        </Main>
+      </div>
+    </OddsProvider>
   );
 }
 
---
+++ src/contexts/oddsContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react';
+
+const OddsContext = createContext(['', () => {}]);
+
+const OddsProvider = ({ children }) => {
+  const [isFractal, setOddsType] = useState(false);
+  return (
+    <OddsContext.Provider value={[isFractal, setOddsType]}>
+      {children}
+    </OddsContext.Provider>
+  );
+};
+
+export { OddsContext, OddsProvider };
