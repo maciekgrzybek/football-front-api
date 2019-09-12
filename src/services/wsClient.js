bb853995b9552b0541f29b02201197ec62ce2a84
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-09-12 22:51:52 +0100

    Last time fixed checking for ready state

--- src/services/wsClient.js
+++ src/services/wsClient.js
@@ -38,57 +38,83 @@ class WebSocketClass {
     });
   }
 
-  checkIfReady(callback) {
-    const connectionCheck = setInterval(() => {
+  checkIfReady() {
+    return new Promise((resolve, reject) => {
       if (this.client.readyState === 1) {
-        clearInterval(connectionCheck);
-        callback()
+        resolve()
+      } else {
+        let timesTried = 0
+        let limit = 500;
+        const connectionCheck = setInterval(() => {
+          if (this.client.readyState === 1) {
+            clearInterval(connectionCheck);
+            resolve();
+          } else if (timesTried > limit) {
+            reject('Connectstion was taking too long. Try restarting the application.')
+          } else {
+            timesTried++;
+          }
+        }, 10);
       }
-    }, 10);
+    })
   }
 
   getEvent(key) {
-    this.checkIfReady(() => {
-      this.client.send(JSON.stringify({ type: 'getEvent', id: key }));
-    });
-    return this.handleResponse(key, 'event');
-
+    return this.checkIfReady()
+      .then(() => {
+        this.client.send(JSON.stringify({ type: 'getEvent', id: key }));
+        return this.handleResponse(key, 'event');
+      })
+      .catch(error => console.log(error));
   }
+
   getEvents() {
-    this.checkIfReady(() => {
-      console.log('e?')
-      this.client.send(JSON.stringify({ type: 'getLiveEvents' }));
-    });
-    return this.handleResponse();
+    return this.checkIfReady()
+      .then(() => {
+        this.client.send(JSON.stringify({ type: 'getLiveEvents' }));
+        return this.handleResponse();
+      })
+      .catch(error => console.log(error));
   }
-  getMarket(key) {
-    this.checkIfReady(() => {
-      this.client.send(JSON.stringify({ type: 'getMarket', id: key }));
-    });
-    return this.handleResponse(key, 'market');
-  }
-  getMarkets(keys) {
-    return this.checkIfReady(() => {
-      return Promise.all(
-        keys.map((market) => {
-          return this.getMarket(market);
-        }));
-    });
 
+  getMarket(key) {
+    return this.checkIfReady()
+      .then(() => {
+        this.client.send(JSON.stringify({ type: 'getMarket', id: key }));
+        return this.handleResponse(key, 'market');
+      })
+      .catch(error => console.log(error));
   }
+
+  getMarkets(keys) {
+    return this.checkIfReady()
+      .then(() => {
+        return Promise.all(
+          keys.map((market) => {
+            return this.getMarket(market);
+          }));
+      })
+      .catch(error => console.log(error));
+  }
+
   getOutcome(key) {
-    this.checkIfReady(() => {
-      this.client.send(JSON.stringify({ type: 'getOutcome', id: key }));
-    });
-    return this.handleResponse(key, 'outcome');
+    return this.checkIfReady()
+      .then(() => {
+        this.client.send(JSON.stringify({ type: 'getOutcome', id: key }));
+        return this.handleResponse(key, 'outcome');
+      })
+      .catch(error => console.log(error));
   }
+
   getOutcomes(keys) {
-    return this.checkIfReady(() => {
-      return Promise.all(
-        keys.map((outcome) => {
-          return this.getOutcome(outcome);
-        }));
-    })
+    return this.checkIfReady()
+      .then(() => {
+        return Promise.all(
+          keys.map((outcome) => {
+            return this.getOutcome(outcome);
+          }));
+      })
+      .catch(error => console.log(error));
   }
 }
 
