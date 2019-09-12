f137112bdc98f4809f997cd18c52d5baaa115571
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-09-12 13:23:58 +0100

    Add single event

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,19 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { Spin } from 'antd';
 
+import EventTitle from '../components/event-title/EventTitle';
+import MarketsList from '../components/markets-list/MarketsList';
 import wsClient from '../services/wsClient';
+import sortMarkets from '../helpers/sortMarkets';
+import filterDisplayable from '../helpers/filterDisplayable';
 
 function Event({ eventId }) {
   const [eventData, setEventData] = useState(null);
+  const [markets, setMarkets] = useState(null);
   useEffect(() => {
-    wsClient.getEvent(eventId).then(data => setEventData(data));
+    const fetchData = async() => {
+      const event = await wsClient.getEvent(Number(eventId));
+      const marketsData = Promise.all(
+        event.data.markets.map((market) => {
+          return wsClient.getMarket(market);
+        }))
+        .then(data => data.filter(filterDisplayable).sort(sortMarkets));
+      
+      setMarkets(await marketsData);
+      setEventData(event.data)
+    };
+    fetchData();
   }, [eventId]);
 
   if (!eventData) {
     return <Spin />;
   }
   return (
-    <div>Event page: {eventId}</div>
+    <div>
+      <EventTitle data={eventData}/>
+      {eventData.linkedEventTypeName && (
+        <h4>{eventData.linkedEventTypeName}</h4>
+      )}
+      {eventData.startTime && (
+        <h4>{eventData.startTime}</h4>
+      )}
+      {
+        markets && <MarketsList markets={markets} />
+      }
+    </div>
   );
 }
 
